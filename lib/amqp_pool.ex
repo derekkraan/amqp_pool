96281978f7d6dbee8e6d306b3bfd4f8a8c43231c
Will R <email@astutecat.dev>
2020-04-30 10:22:29 +0200

    Switch this to being a module that can be included in a supervision tree and allow for runtime configuration of this module.

--- lib/amqp_pool.ex
+++ lib/amqp_pool.ex
@@ -2,7 +2,11 @@ defmodule AMQPPool do
   @moduledoc """
   AMQPPool manages a pool of AMQP channels for you to use.
 
-  You need to configure AMQPPool to tell it how to connect and set some pool settings.
+  # Configuration
+
+  You can specify the default configuration options for all instances of AMQPPool
+  using config.exs:
+
   ```elixir
   # these are the same settings as for poolboy
   config :amqp_pool, :pool_settings,
@@ -16,6 +20,46 @@ defmodule AMQPPool do
     virtual_host: ""
   ```
 
+  You can override these options later when adding AMQPPool to your supervision tree (see below).
+
+  # Running AMQPPool
+
+  To run it, add it to your application's supervision tree:
+
+  ```elixir
+  defmodule MyApplication.SomeSupervisor do
+  use Supervisor
+
+  def start_link(init_arg) do
+    Supervisor.start_link(__MODULE__, init_arg, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_init_arg) do
+      runtime_options = [
+        amqp_connection_settings: [virtual_host: ""],
+        pool_settings: [pool_size: 10, max_overflow: 10]
+      ]
+
+      # The first child uses the default options in config.exs exclusively
+      # The second child here uses options passed in at runtime, which can override some
+      #  or all of the options.
+
+      children = [
+        AMQPPool,
+        {AMQPPool, runtime_options}
+      ]
+
+      Supervisor.init(children, strategy: :one_for_one)
+    end
+  end
+  ```
+
   AMQPPool exports one function for you to use: `AMQPPool.Channel.with_channel/1`.
   """
+
+  @doc false
+  def child_spec(opts) do
+    AMQPPool.Supervisor.child_spec(opts)
+  end
 end
---
+++ lib/amqp_pool/supervisor.ex
@@ -0,0 +1,39 @@
+defmodule AMQPPool.Supervisor do
+  use Supervisor
+
+  @doc false
+  def start_link(opts \\ []) do
+    Supervisor.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(opts) do
+    children = [
+      {AMQPPool.Connection, amqp_connection_settings(opts)},
+      :poolboy.child_spec(:channel, poolboy_config(opts), [])
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  defp poolboy_config(opts) do
+    passed_opts  = Keyword.get(opts, :pool_settings, [])
+    config_opts = Application.fetch_env!(:amqp_pool, :pool_settings)
+
+    settings = Keyword.merge(config_opts, passed_opts)
+
+    [
+      {:name, {:local, :channel}},
+      {:worker_module, AMQPPool.Channel},
+      {:size, settings[:pool_size]},
+      {:max_overflow, settings[:max_overflow]}
+    ]
+  end
+
+  defp amqp_connection_settings(opts) do
+    passed_opts  = Keyword.get(opts, :amqp_connection_settings, [])
+    config_opts = Application.fetch_env!(:amqp_pool, :amqp_connection_settings)
+
+    Keyword.merge(config_opts, passed_opts)
+  end
+end
